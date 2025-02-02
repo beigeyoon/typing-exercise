959adb70dfe251e864a0d9eb54ae1170bb759753
beigeyoon <beige.yoon@gmail.com>
2025-02-03 00:18:42 +0900

    feat. 타이틀 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,15 +5,15 @@ export default function Layout() {
     <div className='h-full flex flex-col relative bg-amber-200'>
       <header>
         <h1 className='text-5xl text-center py-24 font-russo-one'>
-          Endless Short Typing
+          TYPING EXERCISE
         </h1>
       </header>
-      <main className='px-32'>
+      <main className='mx-auto w-[680px]'>
         <Outlet />
       </main>
       <footer className='absolute bottom-0 w-full text-lg text-center py-8 font-jersey'>
         <p>
-          © 2025 Endless Short Typing by yooni
+          © 2025 Typing Exercise by yooni
         </p>
       </footer>
     </div>
